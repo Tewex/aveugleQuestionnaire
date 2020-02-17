495f4f14559eb233ad8c1f463262ebae8faa3ca8
LeandroLeleLeandro <leandro.rsstt@eduge.ch>
2020-02-17 15:49:52 +0100

    correction bug leandro ici leandro je signe de la part de leandro

--- WEB/inc/function.php
+++ WEB/inc/function.php
@@ -1,6 +1,6 @@
 <?php
 
-require "../connexionBd.php";
+require "./bdd/connexionBd.php";
 
 $db = connectDB();
 
@@ -8,5 +8,12 @@ if (session_status() == PHP_SESSION_NONE)
 {
   session_start();
 }
+function addUser($nom,$prenom,$email,$pwd,$pseudo)
+{
+    global $bdd;
+    $password = $prenomStagiaire.$nomStagiaire;
 
+    $insertStagiaire = $bdd->prepare("INSERT INTO stagiaires(FIRSTNAME, LASTNAME, EMAIL, PSWD, PRIVATE_PHONE, SCHOOL_NAME, SCHOOL_DEGREE, REMARKS, ROLES_CODE) VALUES(?,?,?,?,?,?,?,?,?)");
+    $insertStagiaire->execute(array($prenomStagiaire,$nomStagiaire,$emailStagiaire,$telephoneStagiaire,$nomEcoleStagiaire,$degreStagiaire));
+}
 ?>
