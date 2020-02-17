b64d1320c8ca60a3b46ad4a5cade3c82469e1b49
unknown <loic.mtrll@eduge.ch>
2020-02-17 15:21:24 +0100

    jsp

--- WEB/question.php
+++ WEB/question.php
@@ -1,11 +1,28 @@
+<?php
+    
+
+?>
 <!doctype html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
   <title>Question</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
+<?php include "inc/navbar/navbarNotLogged.php";?>
 
+<p>question lol tewex enculé de merde ?</p>
+<table id="customers">
+  <tr>
+    <td><a href="">reponse 1</a></td>
+    <td><a href="">reponse 2</a></td>
+  </tr>
+  <tr>
+    <td><a href="">reponse 3</a></td>
+    <td><a href="">reponse 4</a></td>
+  </tr>
+  </table>
 </body>
 </html>
