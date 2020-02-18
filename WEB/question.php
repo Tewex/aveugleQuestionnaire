b20cf4185a8dfe48d852986707cc43a2a2c2fe66
unknown <loic.mtrll@eduge.ch>
2020-02-18 08:58:36 +0100

    avancement de la page question avec le formulaire

--- WEB/question.php
+++ WEB/question.php
@@ -1,5 +1,19 @@
+<!--
+  Auteur : Loic Métrailler
+  Date : 17.02.2020
+  Projet : Aveugle questionnaire
+  Description : projet de groupe sur un site de questions
+  -->
 <?php
-    
+  require "inc/fuinction.php";
+
+  
+
+  if(filter_has_var(INPUT_POST, 'submit')){
+    $reponseUser = filter_input(INPUT_POST, 'reponse', FILTER_SANITIZE_STRING);
+
+    echo $reponseUser;
+  }
 
 ?>
 <!doctype html>
@@ -12,17 +26,24 @@
 </head>
 <body>
 <?php include "inc/navbar/navbarNotLogged.php";?>
+<div style="margin-left:500px;">
+  <p>question ?</p>
 
-<p>question lol ?</p>
-<table id="customers">
-  <tr>
-    <td><a href="">reponse 1</a></td>
-    <td><a href="">reponse 2</a></td>
-  </tr>
-  <tr>
-    <td><a href="">reponse 3</a></td>
-    <td><a href="">reponse 4</a></td>
-  </tr>
-  </table>
+  <form action="" method="POST">
+    <table>
+      <tr>
+        <td><input type="radio" name="reponse" value="1">Reponse 1</td>
+        <td><input type="radio" name="reponse" value="2">Reponse 2</td>
+      </tr>
+      <tr>
+        <td><input type="radio" name="reponse" value="3">Reponse 3</td>
+        <td><input type="radio" name="reponse" value="4">Reponse 4</td>
+      </tr>
+      <tr>
+      <td colspan="2" align="center"><input type="submit" value="confirmer" name="submit"></td>
+      </tr>
+      </table>
+    </form>
+    </div>
 </body>
 </html>
